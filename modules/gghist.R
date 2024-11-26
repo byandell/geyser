e72433c57164494625eba9dbbef9cefff9f0ded1
byandell <byandell@wisc.edu>
2024-11-26 13:46:30 -0600

    add modules and python drafts

---
+++ modules/gghist.R
@@ -0,0 +1,105 @@
+#' Shiny Server for Geyser Ggplot2 Histogram
+#'
+#' @param id shiny identifier
+#' @param df data frame
+#' @param column column of data frame
+
+#' @return reactive server
+#' @export
+#' @rdname gghistServer
+#' @importFrom shiny bootstrapPage checkboxInput moduleServer NS plotOutput
+#'             renderPlot renderUI selectInput shinyApp sliderInput uiOutput
+#' @importFrom ggplot2 aes after_stat geom_histogram geom_rug ggplot ggitlel
+#'             stat_density xlab
+#' @importFrom rlang .data
+gghistServer <- function(id, df, column) {
+  shiny::moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+    
+    # Output Main Plot
+    output$main_plot <- shiny::renderPlot({
+      p <- ggplot2::ggplot(df) +
+        ggplot2::aes(.data[[column]]) +
+        ggplot2::geom_histogram(
+          ggplot2::aes(y = ggplot2::after_stat(density)),
+          bins = as.numeric(input$n_breaks),
+          color = "black", fill = "white") +
+        ggplot2::xlab("Duration (minutes)") +
+        ggplot2::ggtitle("Geyser eruption duration")
+      
+      if (input$individual_obs) {
+        p <- p + ggplot2::geom_rug()
+      }
+      if (input$density) {
+        shiny::req(input$bw_adjust)
+        p <- p + ggplot2::stat_density(
+          adjust = input$bw_adjust,
+          color = "blue", linewidth = 1, fill = "transparent")
+      }
+      print(p)
+    })
+    
+    # Input Bandwidth Adjustment
+    output$bw_adjust <- shiny::renderUI({
+      if(input$density) {
+        shiny::sliderInput(inputId = ns("bw_adjust"),
+                           label = "Bandwidth adjustment:",
+                           min = 0.2, max = 2, value = 1, step = 0.2)
+      }
+    })
+  })
+}
+#' Shiny Module Input for Geyser Ggplot2 Histogram
+#' @param id identifier for shiny reactive
+#' @return nothing returned
+#' @rdname gghistServer
+#' @export
+gghistInput <- function(id) {
+  ns <- shiny::NS(id)
+  shiny::tagList(
+    shiny::selectInput(inputId = ns("n_breaks"),
+                label = "Number of bins in histogram (approximate):",
+                choices = c(10, 20, 35, 50),
+                selected = 20),
+    shiny::checkboxInput(inputId = ns("individual_obs"),
+                  label = shiny::strong("Show individual observations"),
+                  value = FALSE),
+    
+    shiny::checkboxInput(inputId = ns("density"),
+                  label = shiny::strong("Show density estimate"),
+                  value = FALSE))
+}
+#' Shiny Module UI for Geyser Ggplot2 Histogram
+#' @param id identifier for shiny reactive
+#' @return nothing returned
+#' @rdname gghistServer
+#' @export
+gghistUI <- function(id) {
+  ns <- shiny::NS(id)
+  shiny::uiOutput(ns("bw_adjust"))
+}
+#' Shiny Module Output for Geyser Ggplot2 Histogram
+#' @param id identifier for shiny reactive
+#' @return nothing returned
+#' @rdname gghistServer
+#' @export
+gghistOutput <- function(id) {
+  ns <- shiny::NS(id)
+  shiny::plotOutput(ns("main_plot"), height = "300px")
+}
+#' Shiny Module App for Geyser Ggplot2 Histogram
+#' @return nothing returned
+#' @rdname gghistServer
+#' @export
+gghistApp <- function() {
+  ui <- shiny::bootstrapPage(
+    gghistInput("gghist"), 
+    gghistOutput("gghist"),
+    # Display this only if the density is shown
+    gghistUI("gghist")
+  )
+  server <- function(input, output, session) {
+    gghistServer("gghist", faithful, "eruptions")
+  }
+  shiny::shinyApp(ui, server)
+}
---
+++ python/shiny.py
@@ -0,0 +1,75 @@
+pip install shiny
+
+from shiny import App, render, ui
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde
+
+# Sample data similar to faithful$eruptions
+eruptions = np.random.normal(loc=3.5, scale=1.0, size=272)
+# https://gist.githubusercontent.com/curran/4b59d1046d9e66f2787780ad51a1cd87/raw/9ec906b78a98cf300947a37b56cfe70d01183200/data.tsv
+# https://www.kaggle.com/code/niteshhalai/old-faithful-data-visualisation-and-modelling/input?select=faithful.csv
+# https://www.kaggle.com/datasets/janithwanni/old-faithful/data?select=faithful.csv
+# data=pandas.read_csv('filename.tsv',sep='\t')
+
+app_ui = ui.page_fluid(
+    ui.input_select(
+        "n_breaks",
+        "Number of bins in histogram (approximate):",
+        choices=[10, 20, 35, 50],
+        selected=20
+    ),
+    ui.input_checkbox(
+        "individual_obs",
+        "Show individual observations",
+        value=False
+    ),
+    ui.input_checkbox(
+        "density",
+        "Show density estimate",
+        value=False
+    ),
+    ui.output_plot("main_plot"),
+    ui.output_ui("bw_adjust")
+)
+
+def server(input, output, session):
+    @output
+    @render.plot
+    def main_plot():
+        fig, ax = plt.subplots()
+        n_breaks = input.n_breaks()
+        hist_data = np.histogram(eruptions, bins=n_breaks, density=True)
+        ax.bar(hist_data[1][:-1], hist_data[0], width=np.diff(hist_data[1]), edgecolor='black', align='edge')
+
+        if input.individual_obs():
+            ax.plot(eruptions, np.zeros_like(eruptions), 'r|', markersize=10)
+
+        if input.density():
+            bw_adjust = input.bw_adjust()
+            kde = gaussian_kde(eruptions, bw_method=bw_adjust)
+            x_grid = np.linspace(min(eruptions), max(eruptions), 1000)
+            ax.plot(x_grid, kde(x_grid), color='blue')
+
+        ax.set_xlabel("Duration (minutes)")
+        ax.set_title("Geyser eruption duration")
+        return fig
+
+    @output
+    @render.ui
+    def bw_adjust():
+        if input.density():
+            return ui.input_slider(
+                "bw_adjust",
+                "Bandwidth adjustment:",
+                min=0.2,
+                max=2.0,
+                value=1.0,
+                step=0.2
+            )
+        return None
+
+app = App(app_ui, server)
+
+if __name__ == "__main__":
+    app.run()
