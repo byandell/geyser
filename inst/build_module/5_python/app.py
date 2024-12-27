3238aecebdc5067a2f279a0d19c35f3beb0f3375
byandell <byandell@wisc.edu>
2024-12-27 14:06:26 -0600

    add working app.py using hist2.py

--- inst/build_module/5_python/app.py
+++ inst/build_module/5_python/app.py
@@ -1,68 +1,18 @@
 from shiny import App, render, ui
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import gaussian_kde
-from rpy2 import robjects
 
-# `faithful$eruptions` from R
-eruptions = robjects.r['faithful'][0]
+# run_hist.py
+with open('/Users/brianyandell/Documents/Research/geyser/python/hist2.py', 'r') as file:
+    code = file.read()
+exec(code)
 
 app_ui = ui.page_fluid(
-    ui.input_select(
-        "n_breaks",
-        "Number of bins in histogram (approximate):",
-        choices=[10, 20, 35, 50],
-        selected=20
-    ),
-    ui.input_checkbox(
-        "individual_obs",
-        "Show individual observations",
-        value=False
-    ),
-    ui.input_checkbox(
-        "density",
-        "Show density estimate",
-        value=False
-    ),
-    ui.output_plot("main_plot"),
-    ui.output_ui("bw_adjust")
+    hist_input("geyser"),
+    hist_output("geyser"),
+    hist_ui("geyser")
 )
 
 def server(input, output, session):
-    @output
-    @render.plot
-    def main_plot():
-        fig, ax = plt.subplots()
-        n_breaks = input.n_breaks()
-        hist_data = np.histogram(eruptions, bins=n_breaks, density=True)
-        ax.bar(hist_data[1][:-1], hist_data[0], width=np.diff(hist_data[1]), edgecolor='black', align='edge')
-
-        if input.individual_obs():
-            ax.plot(eruptions, np.zeros_like(eruptions), 'r|', markersize=10)
-
-        if input.density():
-            bw_adjust = input.bw_adjust()
-            kde = gaussian_kde(eruptions, bw_method=bw_adjust)
-            x_grid = np.linspace(min(eruptions), max(eruptions), 1000)
-            ax.plot(x_grid, kde(x_grid), color='blue')
-
-        ax.set_xlabel("Duration (minutes)")
-        ax.set_title("Geyser eruption duration")
-        return fig
-
-    @output
-    @render.ui
-    def bw_adjust():
-        if input.density():
-            return ui.input_slider(
-                "bw_adjust",
-                "Bandwidth adjustment:",
-                min=0.2,
-                max=2.0,
-                value=1.0,
-                step=0.2
-            )
-        return None
+    hist_server("geyser")
 
 app = App(app_ui, server)
 
