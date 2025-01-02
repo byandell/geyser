e51e55d496d4e8f5e641be839db7d13db963162f
byandell <byandell@wisc.edu>
2025-01-02 10:43:55 -0600

    working app_pages.py

---
+++ inst/connect_modules/app_pages.py
@@ -0,0 +1,42 @@
+# app_path <- "inst/connect_modules/app_pages.py"
+# reticulate::py_run_file(app_path)
+
+# context: setup
+from shiny import App, module, reactive, render, ui
+import geyser.io as io
+from geyser.hist import *
+from geyser.gghist import *
+from geyser.ggpoint import *
+import nest_asyncio
+
+nest_asyncio.apply()
+
+# context: ui
+app_ui = ui.page_navbar(
+  ui.nav_panel("ggpoint",
+               ggpoint_input("ggpoint"), 
+               ggpoint_output("ggpoint"),
+               ggpoint_ui("ggpoint")
+  ),
+  ui.nav_panel("gghist",
+               gghist_input("gghist"), 
+               gghist_output("gghist"),
+               gghist_ui("gghist")
+  ),
+  ui.nav_panel("hist",
+               hist_input("hist"), 
+               hist_output("hist"),
+               hist_ui("hist")
+  ),
+  title = "Geyser Python Modules with NavBar"
+)
+
+# context: server
+def app_server(input, output, session):
+    hist_server("hist")
+    gghist_server("gghist")
+    ggpoint_server("ggpoint")
+
+app = App(app_ui, app_server)
+
+io.app_run(app)
