ce74c533a3ba0694fa84da04a1a20b537418c664
byandell <byandell@wisc.edu>
2025-01-08 11:57:01 -0600

    new slidesPython.qmd; modified app_hist.py

--- inst/build_module/5_python/app_hist.py
+++ inst/build_module/5_python/app_hist.py
@@ -2,13 +2,13 @@ from shiny import App, render, ui
 from geyser.hist import hist_server, hist_input, hist_output, hist_ui
 
 app_ui = ui.page_fluid(
-    hist_input("geyser"),
-    hist_output("geyser"),
-    hist_ui("geyser")
+    hist_input("hist"),
+    hist_output("hist"),
+    hist_ui("hist")
 )
 
 def server(input, output, session):
-    hist_server("geyser")
+    hist_server("hist")
 
 app = App(app_ui, server)
 
