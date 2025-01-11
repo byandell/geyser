fc0a9ad9972755ed8f157fb7eadcd8a913b9f503
byandell <byandell@wisc.edu>
2025-01-11 14:00:58 -0600

    commit slidesPython

--- inst/build_module/4_moduleServer/appHist.R
+++ inst/build_module/4_moduleServer/appHist.R
@@ -1,12 +1,12 @@
 library(geyser)
 
-app_ui <- shiny::bootstrapPage(
+appUI <- shiny::bootstrapPage(
   histInput(id = "hist"), 
   histOutput(id = "hist"),
   histUI(id = "hist")
 )
-app_server <- function(input, output, session) {
+appServer <- function(input, output, session) {
   histServer(id = "hist")
 }
-shiny::shinyApp(app_ui, app_server)
+shiny::shinyApp(appUI, appServer)
 
