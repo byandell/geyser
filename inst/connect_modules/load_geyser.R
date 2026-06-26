0040c6da55b58440dae5c1d2176924c7e977f107
Brian S. Yandell <byandell@wisc.edu>
2026-06-26 10:01:16 -0500

    Delete inst/connect_modules/load_geyser.R

--- inst/connect_modules/load_geyser.R
+++
@@ -1,8 +0,0 @@
-# Helper script to load geyser package modules dynamically depending on running environment
-if (dir.exists("R")) {
-  for (f in list.files("R", pattern = "\\.[Rr]$", full.names = TRUE)) source(f)
-} else if (dir.exists("../../R")) {
-  for (f in list.files("../../R", pattern = "\\.[Rr]$", full.names = TRUE)) source(f)
-} else {
-  library(geyser)
-}
